82a5898f7493edf3bc99ac318329a24f6cd25918
Aditya Mishra <vadityamishra777@gmail.com>
2025-12-14 22:37:05 +0530

    Add files via upload

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //it treats all JS code as a newer version 
+
+// alert(3+3)// we are using Node js not Browser so it will not work on NodeJs but work in Browser
+
+console.log("Aditya");
+
+let name = "Adrix"
+let age = 18 
+let isLoggedIn = false
+let state; 
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true / false 
+// null => standalone value 
+// undefined =>
+    // symbol unique
+
+
+
+
+//Object 
+
+console.log(typeof age); // number datatype
+console.log(typeof undefined); // gives undifined
+console.log(typeof null); // gives the object datatype 
+
+
+
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const acountId = 144553
+ let accountEmail = "hellow@gmail.com"
+ var accountPassward = "4554"
+ accountCity ="CHITTORGHAR "
+ let accountState; // Js takes it undefined 
+
+// acountId = 3// not allowed
+
+ accountEmail = "hellw@gmail.com"
+  accountPassward = "554"
+  accountCity ="CHITTORGHAR "
+/*
+Try not to use var because 
+of isseue in block scope and functional scope 
+only use const and val 
+*/
+  //or 
+  console.log(acountId)
+
+  console.table([acountId,accountEmail, accountPassward,accountCity,accountState]) 
